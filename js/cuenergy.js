8a7214b5f172a6d22d350f8a25ab43af928d01cd
David Cutri <dc774@cornell.edu>
2018-04-06 06:29:33 -0400

    css

--- js/cuenergy.js
+++ js/cuenergy.js
@@ -4,8 +4,24 @@ jQuery(document).ready(function($) {
 		
 	// Window Size Tracking
 	function resizeChecks() {
-
+		if ( $(window).width() <= 767 ) {
+			$('#navbar .navbar-header').before('.menu.menu--main.nav.navbar-nav');
+			$('#navbar .navbar-header').css('display', 'none');
+		} else {
+			$('.menu.menu--main.nav.navbar-nav').appendTo('#block-cuenergy-main-menu');
+			$('#navbar .navbar-header').css('display', 'block');
+		}
 	}
+	
+	if ( $(window).width() <= 767 ) {
+		$('#navbar .navbar-header').before('.menu.menu--main.nav.navbar-nav');
+		$('#navbar .navbar-header').css('display', 'none');
+	} else {
+		$('.menu.menu--main.nav.navbar-nav').appendTo('#block-cuenergy-main-menu');
+		$('#navbar .navbar-header').css('display', 'block');
+	}
+	$(window).resize(resizeChecks);
+	resizeChecks();
 	
 	
 	
