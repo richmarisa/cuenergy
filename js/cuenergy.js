9398527d2845e53a408c6240f6dd9a2cebfcf0e2
David Cutri <dc774@cornell.edu>
2018-04-09 12:12:05 -0400

    theme stuff

--- js/cuenergy.js
+++ js/cuenergy.js
@@ -22,7 +22,7 @@ jQuery(document).ready(function($) {
 	}
 	$(window).resize(resizeChecks);
 	resizeChecks();
-	
+		
 	
 	
 	// Window Load ------------------------------------------------------------
