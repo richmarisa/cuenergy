34b1b792b9f3e3e1b3f8afc60e0c20bda80f6c3b
David Cutri <dc774@cornell.edu>
2018-04-11 13:06:01 -0400

    jquery fix

--- js/cuenergy.js
+++ js/cuenergy.js
@@ -26,7 +26,7 @@ jQuery(document).ready(function($) {
 	
 	
 	// Window Load ------------------------------------------------------------
-	$(window).load(function() {
+	$(window).on("load", function() {
 		
 		
 	});
